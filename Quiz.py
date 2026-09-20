a45379f0b0e8a54aca3a1b19460034f3458396f1
jsisodia3110-pixel <jsisodia3110@gmail.com>
2026-09-20 11:46:16 +0530

    Add age eligibility check and balloon feature

--- Quiz.py
+++ Quiz.py
@@ -4,6 +4,7 @@ st.write('hello, I am Jitendra. Welcome to my quiz zone. Hope you like the game.
 age = st.number_input('Enter your age')
 if age>=18:
   st.write('Yu are eligible for license....')
+  st.balloons()
 else:
   st.write('You are not eligible')
 
