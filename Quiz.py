f64675e7f04bcb20cecfd1b391292a490099f187
jsisodia3110-pixel <jsisodia3110@gmail.com>
2026-09-20 11:43:05 +0530

    Replace static age with user input for eligibility

--- Quiz.py
+++ Quiz.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.write('hello, I am Jitendra. Welcome to my quiz zone. Hope you like the game. You may please proceed further for gaming...')
-age = 18
+age = st.number_input('Enter your age')
 if age>=18:
   st.write('Yu are eligible for license....')
 else:
